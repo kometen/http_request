ae66ec004fc1d55f6d596c9d64b2298832bb3ab9
Claus Guttesen <kometen@gmail.com>
2020-09-15 09:58:14 +0200

    Redirect upon 1 HTTP 301 (moved permanently)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate hyper;
 
+use hyper_tls::HttpsConnector;
 use hyper::Client;
 use hyper::body::HttpBody as _;
 use tokio::io::{stdout, AsyncWriteExt as _};
@@ -8,12 +9,23 @@ use tokio::io::{stdout, AsyncWriteExt as _};
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     println!("Jeg æder blåbærsyltetøj!");
 
-    let client = Client::new();
-    let uri = "https://www.freebsd.org/".parse()?;
-    let mut resp = client.get(uri).await?;
-    println!("Response: {}", resp.status());
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    let uri = "https://freebsd.org/".parse()?;
+    let mut res = client.get(uri).await?;
+    println!("Response: {}", res.status());
+    if res.status() == 301 {
+        if res.headers().contains_key("location") {
+            let l = match res.headers().get("location") {
+                Some(l) => l,
+                _ => res.headers().get("location").unwrap()
+            };
+            let uri = std::str::from_utf8(l.as_bytes()).unwrap().to_string().parse()?;
+            res = client.get(uri).await?;
+        }
+    }
 
-    while let Some(chunk) = resp.body_mut().data().await {
+    while let Some(chunk) = res.body_mut().data().await {
         stdout().write_all(&chunk?).await?;
     }
 
